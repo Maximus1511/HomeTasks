c1ad0afa45b2eb7c13963a4676f477b0912d6116
Maksim Kryzhanovskii <krimaxim2010@yandex.ru>
2022-05-24 00:25:19 +0300

    Begining of hometask4

---
+++ AllHomeTasks/src/main/java/lesson4/HomeWorkApp4.java
@@ -0,0 +1,36 @@
+package lesson4;
+
+public class HomeWorkApp4 {
+    public static final int SIZE = 3;
+    public static final String HUMAN_SIGN = "$";
+
+
+    public static void main(String[] args) {
+
+
+        InitializeGame();
+        LaunchGame();
+        EndGame();
+
+    }
+
+    private static void InitializeGame() {
+        InitMap();
+    }
+
+    private static void InitMap() {
+    }
+
+    private static void LaunchGame() {
+
+    }
+
+    private static void EndGame() {
+    }
+
+    private static void MessagetoUser(String str){
+        System.out.println(str);
+    }
+
+  }
+
